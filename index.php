674cb67d5773657b8a5971848bd9ecc2065246ae
bombbovich <surapun.tongrungsi@nectec.or.th>
2023-01-05 13:11:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
   echo "TEST";
   //phpinfo();
-  echo '<html>
-        <head>
-          <a href="graph.html">Visit Graph page</a>
-        </head>
-        <body>
-          TEST GRAPH
-        </body>
-        </html>';
+
 ?>
